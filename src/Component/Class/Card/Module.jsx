71c7b261fea959174d2617c7931fde6e12f3d942
Simlex <gettosimlex@gmail.com>
2023-02-12 23:47:33 +0100

    Added functionality to update video index

--- src/Component/Class/Card/Module.jsx
+++ src/Component/Class/Card/Module.jsx
@@ -6,10 +6,10 @@ const Module = (props) => {
     return(
         <div className="tutor-according-item">
             <div className="tutor-according-head">
-                <h4>{props.title}</h4>
+                <h4>{props.module}</h4>
                 <RiArrowDownSLine />
             </div>
-            <div className="tutor-according-body">
+            <div className="tutor-according-body" onClick={() => props.setCurrentVideoIndex(props.index)}>
                 {/* <span>{props.time}</span> */}
                 <h5>{props.title}</h5>
             </div>
