571d2ccf9874def5151a0be6cd617527ea14002e
Simlex <gettosimlex@gmail.com>
2023-02-25 18:52:56 +0100

    fixed build errors

--- src/Component/Landing Page/PupCourse/PupCourse.jsx
+++ src/Component/Landing Page/PupCourse/PupCourse.jsx
@@ -3,44 +3,42 @@ import "./PupCourse.css";
 import Card from "./Card.jsx";
 // import ClassesData from '../../Courses/Data/ClassesData'
 import { Link } from "react-router-dom";
-// import { useState } from "react";
+import { useState } from "react";
 import MemoryKeys from "../../models/MemoryKeys";
 
 const PupCourse = ({ landingCourses }) => {
-  // const [course, setCourse] = useState();
-  let course;
+  const [course, setCourse] = useState();
 
   const fetchCourses = useCallback(async () => {
     if (course) {
       return;
     }
 
-    let result = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/v1/course`, {
-      method: "get",
-      credencials: "include",
-    });
+    let result = await fetch(
+      `${process.env.REACT_APP_SERVER_URL}/api/v1/course`,
+      {
+        method: "get",
+        credencials: "include",
+      }
+    );
     result = await result.json();
-
-    course = result.data;
-    // setCourse(result.data);
-
+    
     const data = result.data;
+    
+    setCourse(data);
 
     // console.log("RESULT: ", data);
 
     localStorage.setItem(MemoryKeys.Courses, JSON.stringify(data));
 
-    
-    course = result.data;
-    // setCourse(data);
+    data && setCourse(data);
   }, [course]);
 
   useEffect(() => {
     if (!course) {
       fetchCourses();
     }
-    console.log('COURSE DATA: ', course);
-
+    console.log("COURSE DATA: ", course);
   }, [course, fetchCourses]);
 
   return (
