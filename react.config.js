59909c5dae13a3d64d49a07918a1e09ef9683d38
Simlex <gettosimlex@gmail.com>
2023-03-04 23:53:26 +0100

    Changed back end url

--- react.config.js
+++ react.config.js
@@ -1,7 +1,8 @@
 const nextConfig = {
 	reactStrictMode: true,
 	env: {
-		REACT_APP_SERVER_URL: 'https://golearnapi-test.azurewebsites.net/', 
+		// REACT_APP_SERVER_URL: 'http://localhost:3000',
+		REACT_APP_SERVER_URL: 'https://golearntest.azurewebsites.net/', 
 	}
 }
 
