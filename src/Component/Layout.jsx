b64204e99b81542300396631d8909b4a44a5457c
Simlex <gettosimlex@gmail.com>
2023-03-18 23:47:09 +0100

    Updated file name

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./NavBar/Navbar";
+import Navbar from "./NavBar/NavBar";
 
 const Layout = ({ loginStatus }) => {
   return (
